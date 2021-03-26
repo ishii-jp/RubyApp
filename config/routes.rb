f04714f4a1700b2988775692522445bf32e75330
KunitakaIshii <sadaharu5goo@icloud.com>
2021-03-26 18:14:55 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # トップ画面
+  get '/', to: 'index#index', as: 'index'
+
   # ログイン
   get 'login', to: 'sessions#new', as: 'get_login'
   post 'login', to: 'sessions#create', as: 'post_login'
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
   # 新規ユーザー作成
   get 'signup', to: 'users#index', as: 'signup'
   post 'user_create', to: 'users#create', as: 'user_create'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
