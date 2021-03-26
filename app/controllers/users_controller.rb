00d61292b1c0ab68ff325ae74ae8cb0366d392ff
KunitakaIshii <sadaharu5goo@icloud.com>
2021-03-26 18:10:35 +0900

    ユーザー作成機能をひとまず正常系作成しました、後ほどリファクタします

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
+# ユーザー管理クラス
 class UsersController < ApplicationController
+    INFORCE = 0 # 有効
+    INVALID = 1 # 無効
+    USER = 0 # ユーザー
+    ADMIN = 1 # 管理者
+
+    # /signup画面
+    # 新規ユーザー作成画面表示
+    def index
+        # @user=User.new
+        render "signup"
+    end
+
+    # /user_create画面
+    #　新規ユーザー作成機能
+    def create
+        logger.debug('log create_method_start')
+        # TODO バリデート、トランザクション処理、例外処理を行う
+        @user = User.new(
+            name: params[:name],
+            email: params[:email],
+            password_digest: params[:password],
+            admin_flg: USER,
+            status_flg: INFORCE,
+            last_login_timestamp: params[:last_login_timestamp]
+        )
+        if @user.save
+            flash[:msg] = "ユーザー登録が完了しました"
+        else
+            flash[:msg] = "ユーザー登録に失敗しました。\n最初からやり直してください"
+        end
+        redirect_to action: "index"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'login', to: 'session#new'
-  post 'login', to: 'session#create'
-  delete 'logout', to: 'session#destory'
+  # ログイン
+  get 'login', to: 'sessions#new', as: 'get_login'
+  post 'login', to: 'sessions#create', as: 'post_login'
+
+  # ログアウト
+  delete 'logout', to: 'sessions#destory', as: 'logout'
+
+  # 新規ユーザー作成
+  get 'signup', to: 'users#index', as: 'signup'
+  post 'user_create', to: 'users#create', as: 'user_create'
 end
