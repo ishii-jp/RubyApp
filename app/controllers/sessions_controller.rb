1e5212265f6af33239d3ba0b66c01f455fa9fdcd
KunitakaIshii <sadaharu5goo@icloud.com>
2021-03-26 14:54:11 +0900

    ログイン用コントローラを作成

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+# ログイン及びログアウトを行うクラス
+class SessionsController < ApplicationController
+  # ログイン画面
+  def new
+  end
+
+  # ログイン送信
+  def create
+  end
+
+  # ログアウト
+  def destory
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destory'
 end
