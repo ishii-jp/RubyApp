363dc308e80fd401c013eb30922a0237ec3a9170
KunitakaIshii <sadaharu5goo@icloud.com>
2021-03-29 11:35:10 +0900

    コントローラーの前処理を実装しました。

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,4 +16,20 @@ module SessionsHelper
     def is_login?
         !current_user.nil?
     end
+
+    # アクションの実行前にログインしているか確認します。
+    # 呼び出す際に引数がない場合、未ログインの場合は、ログイン画面へリダイレクトします
+    # 引数が設定されると、ログインの場合トップ画面へリダイレクトします。
+    # @param bool conditions_flg if文判定条件
+    def action_before_is_login(conditions_flg = false)
+        if !conditions_flg
+            if !current_user
+                redirect_to get_login_path
+            end
+        else
+            if current_user
+                 redirect_to index_path
+            end
+        end
+    end
 end
