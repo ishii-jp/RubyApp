a5811cf44c918176449872243714cca6605dc95f
KunitakaIshii <sadaharu5goo@icloud.com>
2021-04-05 23:45:14 +0900

    申請作成機能をリファクタリングしました。

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -12,40 +12,23 @@ class RequestsController < ApplicationController
     end
 
     # 申請確認画面
-    # /request/confirm
-    def request_confirm
+    # /request/regist
+    def request_regist
         logger.debug('Log request_confirm method start')
-        # TODO バリデートを行う
-        request = Request.new
-        valid_result = request_valid(request, params)
+        @request = Request.new(
+            params.require(:requests)
+            .permit(:category_id, :price, :body, :approver_id, :confirming)
+            .merge(user_id: current_user.id)
+        )
         
-        if not valid_result
-            @category = params[:categories]['name']
-            @approver_user = params[:approver_user]['name']
-            @price = params[:price]
-            @body = params[:body]
+        if @request.save
+            redirect_to request_path
         else
-            if valid_result.instance_of?(Array)
-                @valid_error_msgs = valid_result
-            end
             @categories = RequestCategory.all
             user = User.new
             @approver_useres = user.get_admin_users
-            render "request_index"
-        end
-    end
 
-    # 申請完了画面
-    # /request/complete
-    def request_complete
-        logger.debug('Log request_complete method start')
-
-        request = Request.new
-        if not request.request_save(request, current_user.id, params)
-            # フラッシュメッセージを設定したあと、request画面を返す
-            render "request_index"
-        else
-            redirect_to request_path
+            render :request_index
         end
     end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -6,18 +6,12 @@ class Request < ApplicationRecord
         validates :price
         validates :body
     end
+    validates_acceptance_of :confirming, allow_nil: false
+    after_validation :check_confirming
 
-    # 申請登録を行います
-    # @param Request request Requestインスタンス
-    # @param int user_id 申請したユーザーID
-    # @param array params フォームから取得した値
-    # @return boolean true:登録成功 false:登録失敗
-    def request_save(request, user_id, params)
-        request.user_id = user_id
-        request.category_id = params[:categories_name]
-        request.approver_id = params[:approver_user]
-        request.price = params[:price]
-        request.body = params[:body]
-        request.save
+    # バリデーション後エラーがなければフォーム登録時確認画面表示用confirmingを設定します
+    def check_confirming
+        errors.delete(:confirming)
+        self.confirming = errors.empty? ? '1': ''
     end
 end
