dab9d4b1dc155c0ae2a88d957939d48153c6d4b2
KunitakaIshii <sadaharu5goo@icloud.com>
2021-05-04 20:45:12 +0900

    requestクラスのテストを作成

--- test/models/request_test.rb
+++ test/models/request_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class RequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "update_approver_id_正常にapprover_idが0から1へ更新されること" do
+    req = Request.find(2) # approver_idが0のテストデータを取得
+    assert_same(req.approval_flg, 0)
+
+    Request.update_approver_id(req.id)
+
+    req_after = Request.find(2)
+
+    assert_same(req_after.approval_flg, Request::APPROVE) # 値が更新されたことの検証
+  end
 end
