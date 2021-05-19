22db2f2ae561f215d6c2ebef2f568d84a406f9f9
KunitakaIshii <sadaharu5goo@icloud.com>
2021-05-19 09:51:20 +0900

    ユーザー情報更新機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@
 class UsersController < ApplicationController
     before_action->{
         action_before_is_login(true)
-    }
+    },
+    only: [:index, :create]
 
     # /signup画面
     # 新規ユーザー作成画面表示
@@ -27,6 +28,32 @@ class UsersController < ApplicationController
         else
             @user = user
             render "signup"
+        end
+    end
+
+    # ユーザー情報一覧画面
+    # /user/show
+    def show
+        logger.debug('Log show method start')
+        logger.debug(@user.inspect)
+        if @user.nil?
+            @user = current_user
+        end
+    end
+
+    # ユーザー情報更新
+    # /user/update
+    def update
+        logger.debug('Log update method start')
+      
+        @user = User.find(current_user.id)
+        @user.name = params[:user][:name]
+        @user.email = params[:user][:email]
+
+        if @user.save(context: :skip_pass)
+            redirect_to user_show_path, notice: '更新が完了しました'
+        else
+            render action: :show
         end
     end
 end
